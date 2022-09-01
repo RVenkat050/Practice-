9f2a7971e463375c947a70760e7b12ddddc914d9
RVenkat050 <111178278+RVenkat050@users.noreply.github.com>
2022-09-02 01:44:40 +0530

    Add files via upload

---
+++ venkat.py
@@ -0,0 +1,238 @@
+# a = "cricket"
+# start_index = 4
+# end_index = start_index + 3
+# part = a[start_index:end_index]
+# print(part)
+# a = "12.5"
+# a = int(a)
+# print(a)
+# place = "Taj mahal"
+# sub_string = place[:5]
+# print(sub_string)
+# a = 10
+# b = "*" * int(a)
+# print(b)
+# a=10.99
+# b=int(a)
+# print(b)
+# a="software developer"
+# b=a[9:]
+# print(b)
+# weight="10"
+# weight=float(weight)
+# print(type(weight))
+# a="python programming"
+# b=len(a[:6]+ a[7:])
+# print(b)
+# m="Hi5 "*4
+# m=m[6:16]
+# print(m)
+# w=input()
+# w=w*2
+# word= w[1:6]
+# print(type(word[3]))
+# from operator import truediv
+
+
+# print(type("False" + True))
+# print(float(len("4" * 2)))
+# a= 2==2
+# print(a)
+# print(type("6"*5))
+# s=int(input())
+# print("given number:" + s)
+# print(int("3"+"3.0"))
+
+
+
+# a="Balloon"
+
+# b = "Ball"
+
+
+# x = a[:3] == b[:3]
+
+# y = a[2:4] != b[2:4]
+
+# z = a[:4] == b[:4]
+
+
+
+# print(((x and y)) or z)
+
+# print(1=="hello")
+# p=("false" == "false")
+# q=("True" !="True")
+# print(p or q)
+# i= 5
+# j=6
+# k=(i and j)
+# l=(i or j)
+# print ( k and l)
+# expression1 = ((23) != 8) 
+# expression2 =(("Hi" * 3) == "Hi Hi Hi")
+
+
+# expression3 = (not (expression1 _____ expression2))
+
+# 4 expression4 = (expression3 and expression1)
+
+# 5 print(expression4)
+
+
+# print(not( 45=="45"))
+
+
+# c="122"
+
+# d= ((x[0]) + (x[:]))
+
+
+# print((x[0] == 1) and (y == "112"))
+
+# print(not(x <=0 ))
+# print(((False != True)  or (True == False)) and (2 > 3))
+from ast import Break
+from email.headerregistry import HeaderRegistry
+from tkinter import N
+
+
+# a=101
+# b= 10
+# if a != b:
+#     print("not equal")
+# marks = 89
+# if marks>=90:
+#     print("A")
+# if marks>=75:
+#     print("B")
+# if marks>=50:
+#     print("C")
+# else:
+#     print("D")
+
+# a="Here"
+# b="HaRe"
+# if a == b:
+#     print("a and b are same")
+# print("a and b are different")
+
+# time=5
+# is_night =(time<=6) or (time>=23)
+# if is_night:
+#     print("sleep")
+# else:
+#     print("work")
+
+
+# a=4
+# b=3
+# if a==b:
+#     print("Equal")
+#     print("inside if")
+
+
+# time=12
+# is_day=(time>=10) and (time <=17)
+# if is_day:
+#     print("good morning")
+# else:
+#     print("good night")
+
+
+# a=95
+# b=100
+# if a<b:
+#     print(a-b)
+# print(a+b)
+# value=23
+# value=str(value)
+# if value[1]==1:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# number=10
+# if number !=10:
+#     number=number+1
+#     print(number)
+# print(number)
+
+
+# first_number = 10
+
+# second_number = 6
+
+# third_number = 3
+
+
+# 1f  first_number >= second_number and  first_number>=third_number:
+#     print("first_number is greater")
+# 1f second number>
+
+# on
+
+# third_number and second_number > first_number:
+
+# print("second_number is greater")
+
+# else:
+
+# print("third_number is greater")
+
+
+# if not(not(not(True))):
+#     print("LIKE")
+# else:
+#     print("dislike")
+
+# word="Hello"
+# sub_string=word[a] + word[b]
+# print(sub_string)
+
+
+
+
+# for i in range(0,101):
+#     if i%2==0:
+#         print(i ,end=',')
+
+
+# num=int(input("enter number"))
+
+# for i in range(1, num):
+#     for j in range(2,i):
+#         if i%j == 0:
+#             print(i)
+#             break
+
+# for i in range(1,10):
+#     print(i,end=',')
+
+
+# n=int(input('Enter the value'))
+# print('enter the value',n)
+# sum=0
+# for i in range(1, n+1):
+#     sum=sum+i
+
+# print(' total sum from 1 to {} is {}'.format(n, sum))
+
+
+
+# name=input('enter the name')
+# print('Enter the name:', name)
+# print(name[: :-1])
+
+# name=input('enter the name')
+# print('Enter the name:', name)
+# reverse=" "
+# for i in name:
+#     reverse=i+ reverse
+
+# print(' Reverse the value of{} is {}'.format(name, reverse))
+
+number=16
+s=number**0.5
+print(s)
